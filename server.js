1833c3f4fd1b1a70a0380173e9d3d4d554bea83c
bngapinsi <bngapinsi@gmail.com>
2026-08-20 21:48:43 +0200

    MongoDB installation

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+require('dotenv').config()
 const express = require('express');
+const mongoose = require('mongoose')
 const routes = require('./routes');
 
 const app = express();
@@ -6,6 +8,10 @@ const PORT = 3000;
 
 app.use(express.json());
 app.use('/', routes);
+
+mongoose.connect(process.env.DB_CONNECTION)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error('MongoDB connection error:', err));
 
 app.listen(PORT, (error) => {
     if (error) {
