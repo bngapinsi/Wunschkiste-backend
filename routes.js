eca357dadd5bc842cf2828871229059c2a4dede0
bngapinsi <bngapinsi@gmail.com>
2026-08-24 00:37:25 +0200

    Create

--- routes.js
+++ routes.js
@@ -9,5 +9,19 @@ router.get('/wuensche', async(req, res) => {
     res.send(allWuensche);
 });
 
+// post one wunsch
+router.post('/members', async(req, res) => {
+    const newWunsch = new Wunsch({
+        titel: req.body.titel,
+        kategorie: req.body.lastname,
+        preis: req.body.email,
+        link: req.body.ipaddress,
+        bildUrl: req.body.bildUrl,
+        notiz: req.body.notiz
+    })
+    await newWunsch.save();
+    res.send(newWunsch);
+});
+
 
 module.exports = router;
