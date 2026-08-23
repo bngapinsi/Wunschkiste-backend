76d145b70186c15cf3582089fd39ea1c24f33744
bngapinsi <bngapinsi@gmail.com>
2026-08-23 21:49:53 +0200

    Model für Wunsch

---
+++ models/wuensche.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+    titel: String,
+    kategorie: String,
+    preis: Number,
+    link: String,
+    bildUrl: String,
+    notiz: String,
+}, {
+    toJSON: {virtuals: true}
+});
+
+module.exports = mongoose.model('Wunsch', schema);
