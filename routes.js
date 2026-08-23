969897e84273ca9bac286f94d9bacdba5bb9948e
bngapinsi <bngapinsi@gmail.com>
2026-08-23 22:03:31 +0200

    Read all

--- routes.js
+++ routes.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const Wunsch = require('./models/wunsch');
+
+// get all wuensche
+router.get('/wuensche', async(req, res) => {
+    const allWuensche = await Wunsch.find();
+    console.log(allWuensche);
+    res.send(allWuensche);
+});
 
 
 module.exports = router;
