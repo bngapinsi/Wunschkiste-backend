bac17bcf8007c3cf8eaaf6942ed23769183fd7b6
bngapinsi <bngapinsi@gmail.com>
2026-08-27 16:49:36 +0200

    Bild Upload

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose')
 const routes = require('./routes');
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 app.use('/', routes);
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 mongoose.connect(process.env.DB_CONNECTION)
   .then(() => console.log('MongoDB connected'))
